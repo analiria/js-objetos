bee9029ffc203b8cfa8dabf360cbf9b81e1b54df
analiria <pires.portes@escola.pr.gov.br>
2026-06-18 10:18:24 -0300

    .

--- aula5/AULA 5/main.py
+++ aula5/AULA 5/main.py
@@ -1,3 +1,15 @@
-url = https://economia.awesomeapi.com.br/analiria/last/USD-BRL
+url = https://economia.awesomeapi.com.br/json/last/USD-BRL
 
- indice_interrrogacao = urb.find('?')
+indice_interrrogacao = urb.find('?')
+url_base = urb[:indice_interrrogacao]
+
+url_parametros = url [indice_interrrogacao + 1:]
+print(urb_parametros)
+
+parametros_busca = 'moedadestino'
+indice_parametro = url_parametros.find(parametros_busca)
+indice_valor = indice_parametro+ len(parametros_busca) + 1
+valor = url_parametros[indice_valor:]
+
+
+print(valor)
