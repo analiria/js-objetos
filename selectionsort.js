56dbb51703229ac244201f34e9158cc49aaa9d9c
analiria <pires.portes@escola.pr.gov.br>
2026-06-30 09:52:32 -0300

    selection

---
+++ selectionsort.js
@@ -0,0 +1,11 @@
+const livros = require('./listalivros');
+const menorvalor = require('./menorvalor');
+
+for(let atual = 0; atual < livros.length; atual++){
+    let menor = menorvalor{livros,atual};
+
+    let livroatual = livros{atual};
+    let livromenorpreco = livro[menor]
+
+    
+}
