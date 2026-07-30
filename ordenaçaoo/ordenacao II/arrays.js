1c541b3b4d0e27c77e6dbbf15fdee3526188902b
analiria <pires.portes@escola.pr.gov.br>
2026-07-30 11:21:17 -0300

    juntalista

---
+++ ordenaçaoo/ordenacao II/arrays.js
@@ -0,0 +1,52 @@
+const edFolha = [
+    {
+        titulo:"PHP",
+        preco: 15
+    },
+    {
+        titulo:"Javascript",
+        preco: 25
+    },
+    {
+        titulo:"java",
+        preco: 30
+    },
+    {
+        titulo:"go",
+        preco: 45
+    },
+    {
+        titulo:"Elixir",
+        preco: 50
+    }
+]
+
+
+const edGalho = [
+    {
+        titulo:"Python",
+        preco: 20
+    },
+    {
+        titulo:"rust",
+        preco: 22
+    },
+    {
+        titulo:"ruby",
+        preco: 28
+    },
+    {
+        titulo:"c#",
+        preco: 33
+    },
+    {
+        titulo:"c++",
+        preco: 35
+    }
+    {
+        titulo:"scala",
+        preco: 40
+    }
+]
+
+module.exports = {edFolha, edGalho}
---
+++ ordenaçaoo/ordenacao II/index.js
@@ -0,0 +1,12 @@
+const {edGalho, edFolha}=require('./arrays');
+
+function juntalista(lista1, lita2){
+    let listafinal = [];
+    let posicaoatuallista1 = 0;
+    let posicaoatuallista2 = 0;
+
+    while(posicaoatuallista1 < lista1.length && posicaoatuallista2 < lista2.length){
+    let produtoatual1 = lista1[posicaoatuallista1]
+    let produtoatual2 = lista2[posicaoatuallista2]
+    }
+}
